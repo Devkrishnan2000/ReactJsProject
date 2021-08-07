407cc82d38b033c769d19bcc84817eff8793f949
Devkrishnan2000 <88578356+Devkrishnan2000@users.noreply.github.com>
2021-08-07 14:58:54 +0530

    Add files via upload

---
+++ src/components/Boot.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Boot() {
+  return (
+    <div className="note">
+      <h1>Which is your favorite fruit ?</h1>
+      <p>My favorite fruit is orange</p>
+    </div>
+  );
+}
+
+export default Boot;
